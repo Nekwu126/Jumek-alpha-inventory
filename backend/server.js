b25c7afc4052ad87a3720f9b766034a8c76e4e9a
Nekwu126 <chienujioke@gmail.com>
2025-11-22 13:21:25 +0100

    update app.use url

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['https://inventory-backend-u69e.onrender.com'],
   credentials: true
 }));
 app.use(express.json());
